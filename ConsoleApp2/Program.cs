954b16ed8bc12ba8cdb9a432c44bb0cae6145caf
Juan Rene Guzman Nunez <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 16:38:11 -0500

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sumaNumero(7, 3));
+
+        }
+
+        static int sumaNumero(int n1, int n2) //=>n1+n2;
+
+        {
+        return n1+n2;
+}
+
+        
+    
+    }
+}
---
+++ ConsoleApp2/funciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Digite un numero; " );
+           int n1= int.Parse( Console.ReadLine());
+
+            Console.WriteLine("Digite un numero; ");
+            int n2 = int.Parse(Console.ReadLine());
+
+
+
+
+
+        }
+
+        
+    
+    }
+}
