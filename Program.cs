f936b628093645ad8e7afb00e254bc62ba284a0e
juanreneguzman <65874546+juanreneguzman@users.noreply.github.com>
2020-05-26 05:34:04 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EJERCICIO_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DIGITE UN NUMERO");
+            int n1 = int.Parse(Console.ReadLine());
+
+            if (n1 == 0)
+            {
+                Console.WriteLine(" El producto de 0 por cualquier número es 0 ");
+            }
+            else
+            {
+                Console.WriteLine("DIGITE UN SEGUNDO NUMERO");
+                int n2 = int.Parse(Console.ReadLine());
+
+                int resultado = n1 * n2;
+
+                Console.WriteLine($"El producto es {resultado}");
+            }
+        }
+
+    }
+}
